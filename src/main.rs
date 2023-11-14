74df6a49446b29131c059cba26110aa201f0df33
Isaac Marovitz <isaacryu@icloud.com>
2023-11-13 21:51:30 -0500

    Get Nintendo Logo buffer + Game name

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fmt::format;
 use std::fs::File;
 use std::io::Read;
 use winit::{event_loop::EventLoop, window::WindowBuilder};
@@ -17,12 +18,21 @@ fn main() -> Result<(), impl std::error::Error> {
 
     file.read_to_end(&mut buffer).expect("Failed to read ROM!");
 
-    println!("{:#04X?}", buffer);
+    let nintendo_logo = &buffer[0x0104..=0x0133];
+
+    // Display Nintendo Logo
+
+    // Run checksum
+
+    let name_data = &buffer[0x0134..=0x0143];
+    let index = name_data.iter().position(|&r| r == 0x00).unwrap();
+    let game_name = std::str::from_utf8(&name_data[0..index]).expect("Failed to get game name!");
+    println!("Starting \"{game_name}\"...");
 
     let event_loop = EventLoop::new().unwrap();
 
     let window = WindowBuilder::new()
-        .with_title("gb-rs")
+        .with_title(format!("gb-rs - {:}", game_name))
         .with_inner_size(winit::dpi::LogicalSize::new(160, 144))
         .build(&event_loop)
         .unwrap();
