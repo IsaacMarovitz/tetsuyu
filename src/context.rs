c902e399e8ca0506d65b3d6d4d5ae16ee158dc8f
Isaac Marovitz <isaacryu@icloud.com>
2024-01-16 09:26:14 -0500

    Move shader to subfolder

--- src/context.rs
+++ src/context.rs
@@ -191,7 +191,7 @@ impl Context {
 
         let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
             label: Some("Shader"),
-            source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
+            source: wgpu::ShaderSource::Wgsl(include_str!("shaders/shader.wgsl").into()),
         });
 
         let render_pipeline_layout =
