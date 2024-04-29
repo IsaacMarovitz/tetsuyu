eadbbf17225f67aadf568707113e3745118b00b1
Isaac Marovitz <isaacryu@icloud.com>
2024-04-29 16:23:59 -0400

    Update wgpu
    winit 0.30 is not yet supported by wgpu

--- src/context.rs
+++ src/context.rs
@@ -240,11 +240,13 @@ impl Context {
             vertex: wgpu::VertexState {
                 module: &shader,
                 entry_point: "vs_main",
+                compilation_options: Default::default(),
                 buffers: &[Vertex::desc()],
             },
             fragment: Some(wgpu::FragmentState {
                 module: &shader,
                 entry_point: "fs_main",
+                compilation_options: Default::default(),
                 targets: &[Some(wgpu::ColorTargetState {
                     format: config.format,
                     blend: Some(wgpu::BlendState {
